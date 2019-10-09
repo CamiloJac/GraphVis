180e7e3e3e5dd03398b656566c682f8ef7ef8876
camilo jacomet <camilojacomet@gmail.com>
2019-10-08 20:48:41 -0700

    Cleaned up code a bit

--- Board.py
+++ Board.py
@@ -11,22 +11,28 @@ red = 195, 75, 75
 #initialize screen display
 screen = pygame.display.set_mode(display_size)
 
+#window title
 pygame.display.set_caption('Graph Visualizer')
 
-clock = pygame.time.Clock()
+#clock = pygame.time.Clock()
 
+#initialize empty graph
 myGraph = Graph.Graph()
 
+#function that gets called when text is entered into text field
 def nodes_func_reaction(event):
     element_value = event.el.get_value()
     print("You Have inserted the following text: ", element_value)
     myGraph.setNodes(int(element_value))
 
+#menu title
 title_element = thorpy.make_text("Graph Visualizer", 20, (255,255,0))
 
+#text field
 numberOfNodes = thorpy.Inserter(name="Nodes:")
 numberOfNodes.scale_to_title()
 
+#how we react to the input for our text field
 nodes_reaction = thorpy.Reaction(reacts_to=thorpy.constants.THORPY_EVENT,
                                 reac_func=nodes_func_reaction,
                                 event_args={"id":thorpy.constants.EVENT_INSERT})
@@ -37,6 +43,7 @@ numberOfEdges = thorpy.Inserter(name="Edges:", value="NOT WORKING")
 numberOfEdges.scale_to_title()
 
 
+#button to generate graph
 generate_graph_button = thorpy.make_button("Generate Graph!", 
                                             func=myGraph.draw_graph, 
                                             params={"screen": screen, 
@@ -45,34 +52,35 @@ generate_graph_button = thorpy.make_button("Generate Graph!",
                                             "startX":420,
                                             "startY":240})
 
-#generate_graph_button_reaction = thorpy.Reaction(reacts_to=thorpy.constants.THORPY_EVENT,
-#                                                reac_func=myGraph.draw_graph(screen, display_size, (200,0), (220, 240)),
-#                                                event_args={"id":thorpy.constants.EVENT_PRESS})
 
-
-
+#quit button
 button = thorpy.make_button("Quit", func=thorpy.functions.quit_func)
 
+#elements of menu
 elements = [title_element, numberOfNodes, numberOfEdges, 
 generate_graph_button, button]
 
-
-
+#set our thorpy gui menu up
 box = thorpy.Box(elements=elements)
+
+#this is how we want our gui to react when input is entered
 box.add_reaction(nodes_reaction)
-#box.add_reaction(generate_graph_button_reaction)
+
+#margins for nice fitment
 box.fit_children(margins=(30,30))
+
 #we regroup all elements on a menu, even if we do not launch the menu
 menu = thorpy.Menu(box)
+
 #important : set the screen as surface for all elements
 for element in menu.get_population():
     element.surface = screen
 
 box.set_main_color((150, 150, 150))
+
 #use the elements normally...
 box.set_topleft((0,0))
 box.set_size((200, 480))
-
 
 def game_loop():
     gameExit = False
@@ -89,5 +97,5 @@ def game_loop():
         menu.play()
         
 
-screen.fill(white)
+
 game_loop()
--- Graph.py
+++ Graph.py
@@ -4,13 +4,18 @@ red = 195, 75, 75
 white = 255, 255, 255
 
 class Graph(object):
+
+    #adj list
     graph_dict={}
 
     def __init__(self, graph_dict=False):
+        '''We can take in a graph, if we do then
+        just assign our graph dict to that one'''
         if graph_dict:
             self.graph_dict = graph_dict
         
     def setNodes(self, nodes):
+        #this is to set nodes, we clear first then set
         self.graph_dict.clear()
         for i in range(nodes):
             self.graph_dict[i] = []
@@ -25,11 +30,11 @@ class Graph(object):
         for node in self.graph_dict:
             for neighbor in self.graph_dict[node]:
                 print("(",node,", ", neighbor, ")")
-    
-
 
     def draw_graph(self, screen, size, topleft, startX, startY):
+        #first clear the canvas
         pygame.draw.rect(screen, white, (topleft, (440, 480)))
+        
         offset = 0
         for node in self.graph_dict:
             pygame.draw.circle(screen, red, (startX + offset, startY + offset), 15)

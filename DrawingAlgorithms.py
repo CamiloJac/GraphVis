319fcd169eb42d6a20cd096fd4a4bc70f9b71081
CamiloJac <camilojacomet@gmail.com>
2019-11-12 12:50:55 -0800

    added barycentre draw

--- DrawingAlgorithms.py
+++ DrawingAlgorithms.py
@@ -18,7 +18,7 @@ def normalize_coordinates(coordinates, x1, y1, x2, y2):
 
         if coordinates[coord][1] < yMin:
             yMin = coordinates[coord][1]
-    
+
     xDiff = x1-xMin
     if xDiff > 0:
         for coord in coordinates:
@@ -31,11 +31,10 @@ def normalize_coordinates(coordinates, x1, y1, x2, y2):
 
     return coordinates
 
-
 def randomDraw(g, x1, y1, x2, y2):
     coordinates = {}
     for node in g.graph_dict:
-        coordinates[node] = [random.randrange(x1, x2, 1), 
+        coordinates[node] = [random.randrange(x1, x2, 1),
                             random.randrange(y1, y2, 1)]
     return coordinates
 
@@ -55,13 +54,13 @@ def spring(g, x1, y1, x2, y2, canvas):
                     ydiff = coordinates[nodeFrom][1] - coordinates[nodeTo][1]
                     d = math.sqrt(xdiff**2 + ydiff**2)
                     currentForce = [0.0, 0.0]
-                    
+
                     if nodeTo in g.graph_dict[nodeFrom]:
                         #if nodeTo is in the neighborhood of nodeFrom
                         #lx = c1 * log d * (Delta x/d)
                         #ly = c1 * log d * (Delta y/d)
                         currentForce = [-c1 * math.log(d/c2) * (xdiff/d),
-                                        -c1 * math.log(d/c2) * (ydiff/d)]                     
+                                        -c1 * math.log(d/c2) * (ydiff/d)]
                     else:
                         #if nodeTo is NOT adjacent to nodeFrom
                         currentForce = [(c3 / d**2) * (xdiff/d),
@@ -80,12 +79,62 @@ def spring(g, x1, y1, x2, y2, canvas):
             coordinates[nodeFrom][1] += c4*(forces[nodeFrom][1])
             xtotal += forces[nodeFrom][0]
             ytotal += forces[nodeFrom][1]
-        print("xtotal: ", xtotal, " ytotal: ", ytotal)
+        #print("xtotal: ", xtotal, " ytotal: ", ytotal)
         #just get a copy of the coordinates (floored) for displaying
         coordinatesCopy = floor_coordinates(coordinates)
         coordinatesCopy = normalize_coordinates(coordinates, x1, y1, x2, y2)
         canvas.display_graph(g, coordinatesCopy)
         time.sleep(.025)
 
-    return coordinates    
+    return coordinates
 
+def getPolygon(g, center):
+    sublist = [node for node in g.graph_dict]
+    sublist = sublist[0:6]
+    partition = {}
+    for node in sublist:
+        partition[node] = [0.0, 0.0]
+
+    i = 0
+    for node in partition:
+        partition[node] = [math.ceil(center[0] + 100 * math.cos(i * (2 * math.pi) / len(partition))),
+                            math.ceil(center[1] + 100 * math.sin(i * (2 * math.pi) / len(partition)))]
+        i+=1
+    return partition
+
+def sumEdges(g, nodeV, coordinates):
+    summationX = 0
+    summationY = 0
+    for nodeU in g.graph_dict[nodeV]:
+        summationX += coordinates[nodeU][0]
+        summationY += coordinates[nodeU][1]
+    return [summationX, summationY]
+
+def barycenterDraw(g, x1, y1, x2, y2, canvas):
+    center = [420, 240]
+    coordinates = getPolygon(g, center)
+    fixedVertices = coordinates.copy()
+
+    #place each free vertex at origin
+    for node in g.graph_dict:
+        if node in fixedVertices:
+            continue
+        else:
+            coordinates[node] = center.copy()
+
+    #place free vertices according to fixed vertices
+    for node in g.graph_dict:
+        if node in fixedVertices:
+            continue
+        else:
+            summation = sumEdges(g, node, coordinates)
+            oneOverDeg = 1 / len(g.graph_dict[node])
+            coordinates[node][0] = math.ceil(oneOverDeg * summation[0])
+            coordinates[node][1] = math.ceil(oneOverDeg * summation[1])
+
+    return coordinates
+
+#convex hull
+#tree drawings -- put leaves on outside
+#start with barycentic and plug into spring
+#clean up (try out for more graphs)
